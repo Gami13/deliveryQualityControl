60e9c376276aacf926aeb3fa1ad5f079398fa951
Gami13 <gamiofficial0@gmail.com>
2023-05-18 13:36:55 +0200

    jes wery gud

---
+++ JesonToSqueel.js
@@ -0,0 +1,497 @@
+data = {
+  code: 'PN-EN 10024',
+  name: 'DWUTEOWNIKI STALOWE Z POCHYLONĄ WEWNĘTRZNĄ POWIERZCHNIĄ STOPEK WALCOWANE NA GORĄCO',
+  properties: [
+    {
+      property: 'prostość',
+      propertySymbol: 'qyy',
+      propertyUnit: 'mm',
+      isSecondProperty: true,
+      propertySecond: 'prostość',
+      propertySecondSymbol: 'qxx',
+      propertiesOperation: '',
+      ranges: [
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'wysokość',
+          rangePropertySymbol: 'h',
+          toleranceProperty: 'długość',
+          tolerancePropertySymbol: 'L',
+          canAgreeTolerance: false,
+          rangeFrom: '80',
+          rangeTo: '180',
+          toleranceStart: '0',
+          toleranceEnd: '0.3',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'wysokość',
+          rangePropertySymbol: 'h',
+          toleranceProperty: 'długość',
+          tolerancePropertySymbol: 'L',
+          canAgreeTolerance: false,
+          rangeFrom: '180',
+          rangeTo: '360',
+          toleranceStart: '0',
+          toleranceEnd: '0.15',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'wysokość',
+          rangePropertySymbol: 'h',
+          toleranceProperty: 'długość',
+          tolerancePropertySymbol: 'L',
+          canAgreeTolerance: false,
+          rangeFrom: '360',
+          rangeTo: 'NULL',
+          toleranceStart: '0',
+          toleranceEnd: '0.1',
+        },
+      ],
+    },
+    {
+      property: 'wysokość',
+      propertySymbol: 'h',
+      propertyUnit: 'mm',
+      isSecondProperty: false,
+      ranges: [
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'wysokość',
+          rangePropertySymbol: 'h',
+          toleranceProperty: 'wysokość',
+          tolerancePropertySymbol: 'h',
+          canAgreeTolerance: false,
+          rangeFrom: '0',
+          rangeTo: '200',
+          toleranceStart: '2',
+          toleranceEnd: '2',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'wysokość',
+          rangePropertySymbol: 'h',
+          toleranceProperty: 'wysokość',
+          tolerancePropertySymbol: 'h',
+          canAgreeTolerance: false,
+          rangeFrom: '200',
+          rangeTo: '400',
+          toleranceStart: '3',
+          toleranceEnd: '3',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'wysokość',
+          rangePropertySymbol: 'h',
+          toleranceProperty: 'wysokość',
+          tolerancePropertySymbol: 'h',
+          canAgreeTolerance: false,
+          rangeFrom: '400',
+          rangeTo: 'NULL',
+          toleranceStart: '4',
+          toleranceEnd: '4',
+        },
+      ],
+    },
+    {
+      property: 'szerokość stopki',
+      propertySymbol: 'b',
+      propertyUnit: 'mm',
+      isSecondProperty: false,
+      ranges: [
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'szerokość stopki',
+          rangePropertySymbol: 'b',
+          toleranceProperty: 'szerokość stopki',
+          tolerancePropertySymbol: 'b',
+          canAgreeTolerance: false,
+          rangeFrom: '0',
+          rangeTo: '75',
+          toleranceStart: '1.5',
+          toleranceEnd: '1.5',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'szerokość stopki',
+          rangePropertySymbol: 'b',
+          toleranceProperty: 'szerokość stopki',
+          tolerancePropertySymbol: 'b',
+          canAgreeTolerance: false,
+          rangeFrom: '75',
+          rangeTo: '100',
+          toleranceStart: '2.0',
+          toleranceEnd: '2.0',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'szerokość stopki',
+          rangePropertySymbol: 'b',
+          toleranceProperty: 'szerokość stopki',
+          tolerancePropertySymbol: 'b',
+          canAgreeTolerance: false,
+          rangeFrom: '100',
+          rangeTo: '125',
+          toleranceStart: '2.5',
+          toleranceEnd: '2.5',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'szerokość stopki',
+          rangePropertySymbol: 'b',
+          toleranceProperty: 'szerokość stopki',
+          tolerancePropertySymbol: 'b',
+          canAgreeTolerance: false,
+          rangeFrom: '125',
+          rangeTo: 'NULL',
+          toleranceStart: '3.0',
+          toleranceEnd: '3.0',
+        },
+      ],
+    },
+    {
+      property: 'grubość stopki',
+      propertySymbol: 't',
+      propertyUnit: 'mm',
+      isSecondProperty: false,
+      ranges: [
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'grubość stopki',
+          rangePropertySymbol: 't',
+          toleranceProperty: 'grubość stopki',
+          tolerancePropertySymbol: 't',
+          canAgreeTolerance: false,
+          rangeFrom: '0',
+          rangeTo: '7',
+          toleranceStart: '0.5',
+          toleranceEnd: '1.5',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'grubość stopki',
+          rangePropertySymbol: 't',
+          toleranceProperty: 'grubość stopki',
+          tolerancePropertySymbol: 't',
+          canAgreeTolerance: false,
+          rangeFrom: '7',
+          rangeTo: '10',
+          toleranceStart: '1',
+          toleranceEnd: '2',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'grubość stopki',
+          rangePropertySymbol: 't',
+          toleranceProperty: 'grubość stopki',
+          tolerancePropertySymbol: 't',
+          canAgreeTolerance: false,
+          rangeFrom: '10',
+          rangeTo: '20',
+          toleranceStart: '1.5',
+          toleranceEnd: '2.5',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'grubość stopki',
+          rangePropertySymbol: 't',
+          toleranceProperty: 'grubość stopki',
+          tolerancePropertySymbol: 't',
+          canAgreeTolerance: false,
+          rangeFrom: '20',
+          rangeTo: 'NULL',
+          toleranceStart: '2',
+          toleranceEnd: '2.5',
+        },
+      ],
+    },
+    {
+      property: 'grubość środnika',
+      propertySymbol: 's',
+      propertyUnit: 'mm',
+      isSecondProperty: false,
+      ranges: [
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'grubość środnika',
+          rangePropertySymbol: 's',
+          toleranceProperty: 'grubość środnika',
+          tolerancePropertySymbol: 's',
+          canAgreeTolerance: false,
+          rangeFrom: '0',
+          rangeTo: '7',
+          toleranceStart: '1',
+          toleranceEnd: '0.5',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'grubość środnika',
+          rangePropertySymbol: 's',
+          toleranceProperty: 'grubość środnika',
+          tolerancePropertySymbol: 's',
+          canAgreeTolerance: false,
+          rangeFrom: '7',
+          rangeTo: '10',
+          toleranceStart: '1.5',
+          toleranceEnd: '0.7',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'grubość środnika',
+          rangePropertySymbol: 's',
+          toleranceProperty: 'grubość środnika',
+          tolerancePropertySymbol: 's',
+          canAgreeTolerance: false,
+          rangeFrom: '10',
+          rangeTo: 'NULL',
+          toleranceStart: '2',
+          toleranceEnd: '1',
+        },
+      ],
+    },
+    {
+      property: 'skośność stopek',
+      propertySymbol: 'k',
+      propertyUnit: 'mm',
+      isSecondProperty: true,
+      propertySecond: 'skośność stopek',
+      propertySecondSymbol: 'k`',
+      propertiesOperation: '',
+      ranges: [
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'szerokość stopki',
+          rangePropertySymbol: 'b',
+          toleranceProperty: 'szerokość stopki',
+          tolerancePropertySymbol: 'b',
+          canAgreeTolerance: false,
+          rangeFrom: '0',
+          rangeTo: '100',
+          toleranceStart: '0',
+          toleranceEnd: '2',
+        },
+        {
+          rangeUnit: '%',
+          rangeProperty: 'szerokość stopki',
+          rangePropertySymbol: 'b',
+          toleranceProperty: 'szerokość stopki',
+          tolerancePropertySymbol: 'b',
+          canAgreeTolerance: false,
+          rangeFrom: '100',
+          rangeTo: 'NULL',
+          toleranceStart: '0',
+          toleranceEnd: '2',
+        },
+      ],
+    },
+    {
+      property: 'niesymetryczność stopek',
+      propertySymbol: 'e',
+      propertyUnit: 'mm',
+      isSecondProperty: false,
+      ranges: [
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'szerokość stopki',
+          rangePropertySymbol: 'b',
+          toleranceProperty: 'niesymetryczność stopek',
+          tolerancePropertySymbol: 'e',
+          canAgreeTolerance: false,
+          rangeFrom: '0',
+          rangeTo: '100',
+          toleranceStart: '0',
+          toleranceEnd: '2',
+        },
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'szerokość stopki',
+          rangePropertySymbol: 'b',
+          toleranceProperty: 'niesymetryczność stopek',
+          tolerancePropertySymbol: 'e',
+          canAgreeTolerance: false,
+          rangeFrom: '100',
+          rangeTo: 'NULL',
+          toleranceStart: '0',
+          toleranceEnd: '3',
+        },
+      ],
+    },
+    {
+      property: 'masa',
+      propertySymbol: 'm',
+      propertyUnit: 'mm',
+      isSecondProperty: false,
+      ranges: [
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'masa',
+          rangePropertySymbol: 'm',
+          toleranceProperty: 'masa',
+          tolerancePropertySymbol: 'm',
+          canAgreeTolerance: false,
+          rangeFrom: '0',
+          rangeTo: 'NULL',
+          toleranceStart: '4',
+          toleranceEnd: '4',
+        },
+      ],
+    },
+    {
+      property: 'długość',
+      propertySymbol: 'L',
+      propertyUnit: 'mm',
+      isSecondProperty: false,
+      ranges: [
+        {
+          rangeUnit: 'mm',
+          rangeProperty: 'długość',
+          rangePropertySymbol: 'L',
+          toleranceProperty: 'długość',
+          tolerancePropertySymbol: 'L',
+          canAgreeTolerance: true,
+          toleranceStartAgreement: '50',
+          toleranceEndAgreement: '100',
+          rangeFrom: '0',
+          rangeTo: 'NULL',
+          toleranceStart: '50',
+          toleranceEnd: '50',
+        },
+      ],
+    },
+  ],
+};
+
+console.log(data);
+/*INSERT INTO dbo.elements(
+	code,name,property,propertySymbol,propertySecond, propertySecondSymbol, propertiesOperation, propertyUnit,rangeFrom,rangeTo,rangeUnit,rangeProperty,rangePropertySymbol,toleranceStart,toleranceEnd,toleranceUnit,toleranceProperty,tolerancePropertySymbol,illustration,toleranceStartAgreement,toleranceEndAgreement
+	) VALUES*/
+function str(inp) {
+  return "'" + inp + "'";
+}
+if (data.code == undefined) {
+  data.code = 'NULL';
+} else {
+  data.code = str(data.code);
+}
+if (data.name == undefined) {
+  data.name = 'NULL';
+} else {
+  data.name = str(data.name);
+}
+for (i in data.properties) {
+  let property = data.properties[i].property;
+  let propertySymbol = data.properties[i].propertySymbol;
+  let propertyUnit = data.properties[i].propertyUnit;
+  let isSecondProperty = data.properties[i].isSecondProperty;
+  let propertySecond = data.properties[i].propertySecond;
+  let propertySecondSymbol = data.properties[i].propertySecondSymbol;
+  let propertiesOperation = data.properties[i].propertiesOperation;
+  let illustration = data.illustration;
+
+  if (property == undefined) {
+    property = 'NULL';
+  } else {
+    property = str(property);
+  }
+  if (propertySymbol == undefined) {
+    propertySymbol = 'NULL';
+  } else {
+    propertySymbol = str(propertySymbol);
+  }
+  if (propertySecond == undefined) {
+    propertySecond = 'NULL';
+  } else {
+    propertySecond = str(propertySecond);
+  }
+  if (propertySecondSymbol == undefined) {
+    propertySecondSymbol = 'NULL';
+  } else {
+    propertySecondSymbol = str(propertySecondSymbol);
+  }
+  if (propertiesOperation == undefined) {
+    propertiesOperation = 'NULL';
+  } else {
+    propertiesOperation = str(propertiesOperation);
+  }
+
+  if (propertyUnit == undefined) {
+    propertyUnit = 'NULL';
+  } else {
+    propertyUnit = str(propertyUnit);
+  }
+  if (illustration == undefined) {
+    illustration = 'NULL';
+  } else {
+    illustration = str(illustration);
+  }
+  for (j in data.properties[i].ranges) {
+    let rangeUnit = data.properties[i].ranges[j].rangeUnit;
+    let rangeProperty = data.properties[i].ranges[j].rangeProperty;
+    let rangePropertySymbol = data.properties[i].ranges[j].rangePropertySymbol;
+    let toleranceProperty = data.properties[i].ranges[j].toleranceProperty;
+    let tolerancePropertySymbol =
+      data.properties[i].ranges[j].tolerancePropertySymbol;
+    let canAgreeTolerance = data.properties[i].ranges[j].canAgreeTolerance;
+    let rangeFrom = data.properties[i].ranges[j].rangeFrom;
+    let rangeTo = data.properties[i].ranges[j].rangeTo;
+    let toleranceStart = data.properties[i].ranges[j].toleranceStart;
+    let toleranceEnd = data.properties[i].ranges[j].toleranceEnd;
+    let toleranceStartAgreement =
+      data.properties[i].ranges[j].toleranceStartAgreement;
+    let toleranceEndAgreement =
+      data.properties[i].ranges[j].toleranceEndAgreement;
+    //change all undefined and NULL to NULL
+
+    if (rangeFrom == undefined) {
+      rangeFrom = 'NULL';
+    }
+    if (rangeTo == undefined) {
+      rangeTo = 'NULL';
+    }
+    if (rangeUnit == undefined) {
+      rangeUnit = 'NULL';
+    } else {
+      rangeUnit = str(rangeUnit);
+    }
+    if (rangeProperty == undefined) {
+      rangeProperty = 'NULL';
+    } else {
+      rangeProperty = str(rangeProperty);
+    }
+    if (rangePropertySymbol == undefined) {
+      rangePropertySymbol = 'NULL';
+    } else {
+      rangePropertySymbol = str(rangePropertySymbol);
+    }
+    if (toleranceStart == undefined) {
+      toleranceStart = 'NULL';
+    }
+    if (toleranceEnd == undefined) {
+      toleranceEnd = 'NULL';
+    }
+    if (toleranceProperty == undefined) {
+      toleranceProperty = 'NULL';
+    } else {
+      toleranceProperty = str(toleranceProperty);
+    }
+    if (tolerancePropertySymbol == undefined) {
+      tolerancePropertySymbol = 'NULL';
+    } else {
+      tolerancePropertySymbol = str(tolerancePropertySymbol);
+    }
+
+    if (toleranceStartAgreement == undefined) {
+      toleranceStartAgreement = 'NULL';
+    }
+    if (toleranceEndAgreement == undefined) {
+      toleranceEndAgreement = 'NULL';
+    }
+
+    let sql = `INSERT INTO dbo.elements(
+	code,name,property,propertySymbol,propertySecond, propertySecondSymbol, propertiesOperation, propertyUnit,rangeFrom,rangeTo,rangeUnit,rangeProperty,rangePropertySymbol,toleranceStart,toleranceEnd,toleranceUnit,toleranceProperty,tolerancePropertySymbol,illustration,toleranceStartAgreement,toleranceEndAgreement
+	) VALUES(
+	${data.code},${data.name},${property},${propertySymbol},${propertySecond},${propertySecondSymbol},${propertiesOperation},${propertyUnit},${rangeFrom},${rangeTo},${rangeUnit},${rangeProperty},${rangePropertySymbol},${toleranceStart},${toleranceEnd},${rangeUnit},${toleranceProperty},${tolerancePropertySymbol},${illustration},${toleranceStartAgreement},${toleranceEndAgreement}
+	)`;
+    console.log(sql);
+  }
+}
