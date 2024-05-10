ac3be26f7d3055f56acfdba0af0e4c95ce0e5afc
SeKaFi69 <92319044+SeKaFi69@users.noreply.github.com>
2024-05-10 11:15:21 +0200

    test

--- Mobile/test/Validation.test.ts
+++ Mobile/test/Validation.test.ts
@@ -76,7 +76,79 @@ describe("Validation testing", () => {
 		const result: boolean | Value[] = Validate(element, values);
 		expect(result).toBe(true);
 	});
-
+	it("Validating PNEN10024 - Should Pass", () => {
+		const element = PNEN10024;
+		const values: Value[] = [
+			{
+				ordered: 200,
+				measured: 202,
+				symbol: "h",
+				property: "wysokość",
+			},
+			{
+				ordered: 0,
+				measured: 0.5,
+				symbol: "qyy",
+				property: "prostość",
+			},
+			{
+				ordered: 0,
+				measured: 0.5,
+				symbol: "qxx",
+				property: "prostość",
+			},
+			{
+				ordered: 100,
+				measured: 102,
+				symbol: "b",
+				property: "szerokość stopki",
+			},
+			{
+				ordered: 20,
+				measured: 22.5,
+				symbol: "t",
+				property: "grubość stopki",
+			},
+			{
+				ordered: 7,
+				measured: 6,
+				symbol: "s",
+				property: "grubość środnika",
+			},
+			{
+				ordered: 0,
+				measured: 1,
+				symbol: "k",
+				property: "skośność stopek",
+			},
+			{
+				ordered: 0,
+				measured: 1,
+				symbol: "k`",
+				property: "skośność stopek",
+			},
+			{
+				ordered: 0,
+				measured: 1,
+				symbol: "e",
+				property: "niesymetryczność stopek",
+			},
+			{
+				ordered: 100,
+				measured: 104,
+				symbol: "m",
+				property: "masa",
+			},
+			{
+				ordered: 1703,
+				measured: 1653,
+				symbol: "L",
+				property: "długość",
+			},
+		];
+		const result: boolean | Value[] = Validate(element, values);
+		expect(result).toBe(true);
+	});
 	it("Validating PNEN10024 - Dumb", () => {
 		const element = PNEN10024;
 		const values: Value[] = [
@@ -117,19 +189,19 @@ describe("Validation testing", () => {
 				property: "grubość środnika",
 			},
 			{
-				ordered: 1,
+				ordered: 0,
 				measured: 1,
 				symbol: "k",
 				property: "skośność stopek",
 			},
 			{
-				ordered: 1,
+				ordered: 0,
 				measured: 1,
 				symbol: "k`",
 				property: "skośność stopek",
 			},
 			{
-				ordered: 1,
+				ordered: 0,
 				measured: 1,
 				symbol: "e",
 				property: "niesymetryczność stopek",
@@ -150,4 +222,5 @@ describe("Validation testing", () => {
 		const result: boolean | Value[] = Validate(element, values);
 		expect(result).toBe(true);
 	});
+	
 });
